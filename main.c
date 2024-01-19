d15fcb17541659357539ec46cf58428691d28f43
gary <572981033@qq.com>
2024-01-19 20:20:39 +0800

    add gpio

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-/* 
- *  main.c - Application main entry point 
+/*
+ *  main.c - Application main entry point
  */
 #include <zephyr/types.h>
 #include <stddef.h>
@@ -10,25 +10,132 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <soc.h>
-
 #include "display.h"
-
 #include <zephyr/logging/log.h>
+#include <zephyr/device.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/__assert.h>
 LOG_MODULE_REGISTER(main, 3);
-
+/* size of stack area used by each thread */
 #define STACKSIZE 1024
+
+/* scheduling priority used by each thread */
 #define PRIORITY 7
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*---------------------------------------------------------------------------*/
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+#if !DT_NODE_HAS_STATUS(LED1_NODE, okay)
+#error "Unsupported board: led1 devicetree alias is not defined"
+#endif
+
+struct printk_data_t
+{
+    void *fifo_reserved; /* 1st word reserved for use by fifo */
+    uint32_t led;
+    uint32_t cnt;
+};
+
+K_FIFO_DEFINE(printk_fifo);
+
+struct led
+{
+    struct gpio_dt_spec spec;
+    uint8_t num;
+};
+
+static const struct led led0 = {
+    .spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+    .num = 0,
+};
+
+static const struct led led1 = {
+    .spec = GPIO_DT_SPEC_GET_OR(LED1_NODE, gpios, {0}),
+    .num = 1,
+};
+
+void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
+{
+    const struct gpio_dt_spec *spec = &led->spec;
+    int cnt = 0;
+    int ret;
+
+    if (!device_is_ready(spec->port))
+    {
+        printk("Error: %s device is not ready\n", spec->port->name);
+        return;
+    }
+
+    ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT);
+    if (ret != 0)
+    {
+        printk("Error %d: failed to configure pin %d (LED '%d')\n",
+               ret, spec->pin, led->num);
+        return;
+    }
+
+    while (1)
+    {
+        gpio_pin_set(spec->port, spec->pin, cnt % 2);
+
+        struct printk_data_t tx_data = {.led = id, .cnt = cnt};
+
+        size_t size = sizeof(struct printk_data_t);
+        char *mem_ptr = k_malloc(size);
+        __ASSERT_NO_MSG(mem_ptr != 0);
+
+        memcpy(mem_ptr, &tx_data, size);
+
+        k_fifo_put(&printk_fifo, mem_ptr);
+
+        k_msleep(sleep_ms);
+        cnt++;
+    }
+}
+
+void blink0(void)
+{
+    blink(&led0, 100, 0);
+}
+
+void blink1(void)
+{
+    blink(&led1, 1000, 1);
+}
+
+void uart_out(void)
+{
+
+    while (1)
+    {
+        struct printk_data_t *rx_data = k_fifo_get(&printk_fifo,
+                                                   K_FOREVER);
+        printk("Toggled led%d; counter=%d\n",
+               rx_data->led, rx_data->cnt);
+        k_free(rx_data);
+    }
+}
+// main任务 天然有的任务 不用K_THREAD_DEFINE创建
 int main(void)
 {
     LOG_INF("%s", __func__);
 
     display_init();
-    
+
     display_play();
 }
+// 灯0闪 任务 闪后发出消息
+K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
 
+// 灯1闪 任务 闪后发出消息
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
 
+// 消息接收并打印任务，消息发出一定要及时消化掉 不然会死机
+K_THREAD_DEFINE(uart_out_id, STACKSIZE, uart_out, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
